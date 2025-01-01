0128a48d4e22e979f205412e875b9947267154c1
Sithum Bimsara <sithum.22@cse.mrt.ac.lk>
2025-01-01 19:23:50 +0530

    socket-io part completed

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
+import {app, server} from './socket/socket.js';
 
 
 dotenv.config();
 
-const app = express();
+
 const PORT = process.env.PORT || 5000
 
 
@@ -31,7 +31,7 @@ app.use("/api/users", userRoutes);
 
 
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)
 });
