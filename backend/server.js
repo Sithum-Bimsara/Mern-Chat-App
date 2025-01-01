07d8acba9a6ab6ab39dc997fe5dfef8378cfa92c
Sithum Bimsara <sithum.22@cse.mrt.ac.lk>
2025-01-01 19:57:41 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser'
@@ -16,6 +17,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser());
@@ -24,11 +26,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello World!");
-// });
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*" , (req,res) => {
+    res.sendFile(path.jojn(__dirname, "frontend" , "dist" , "index.html"));
+})
 
 
 server.listen(PORT,() => {
